c2c0e547c02f5473cdbc244eb086b96d228e4200
Linus J <30862662+linus-jansson@users.noreply.github.com>
2021-03-03 12:22:04 +0100

    more nonsense

--- Programering 1/Uppgifter/Lightning on a ball/program.py
+++ Programering 1/Uppgifter/Lightning on a ball/program.py
@@ -9,6 +9,7 @@
 # ’-’ markerar en ganska ljus punkt 0.5 < b ≤ 0.7
 # ’.’ markerar en ljus punkt 0.7 < b ≤ 0.9
 # ’ ’ markerar en mycket ljus punkt 0.9 < b ≤ 1
+# x0 & y0 : positionen på ljuskällan
 ###############################################
 
 import math
@@ -19,11 +20,26 @@ yn = float(input("Bestäm y0 > ")) # y0
 
 zn = ( r**2 - xn**2 - yn**2 )**(1/2) # z0
 
+print(zn)
+
 pk = 1 # proportionalitetskonstanten
 
 x = 0
 y = 0
+
+# if negative:
+#     b = 0
+
 z = ( r**2 - x**2 - y**2 )**(1/2)
  
 
 b = (x * xn + yn + z + zn) / (r*r)
+
+def calculate_sphere():
+    pass
+
+def draw_sphere():
+    pass
+
+def calculate_rays():
+    pass
