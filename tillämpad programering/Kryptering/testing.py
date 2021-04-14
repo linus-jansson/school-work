e53846e76a44ceca0a8520837320dc6f407cbaf4
Linus Jansson <linus.jansson2000@gmail.com>
2021-04-14 12:51:50 +0200

    Added a transpose reverse function which will be tested to unchipher/decrypt a transposed message

--- tillämpad programering/Kryptering/testing.py
+++ tillämpad programering/Kryptering/testing.py
@@ -22,14 +22,49 @@
 
 import math
 
-def flipMatrix(txt):
+def transposeReverse(txt):
+    rows = 3
+    cols = math.ceil(len(txt) / rows)
+    
+    arr = [["" for n in range(cols)] for m in range(rows)]
+    outArr = [["" for n in range(rows)] for m in range(cols)] # Skapar en array/lista som är omvänd jämfört med orginella listan
+    outTxt = ""
+
+    currentChar = 0
+    rCount = 0
+    cCount = 0
+
+    # För varje karaktär, fyll ut listan
+    while currentChar <= len(txt) - 1:
+        # Om raden är slut gå till nästa rad kolumn 0
+        if len(arr[0]) - 1 < cCount:
+            rCount += 1
+            cCount = 0
+        
+        arr[rCount][cCount] = txt[currentChar]
+        cCount += 1
+        currentChar += 1
+        # print(arr, arr[rCount][cCount], txt[currentChar])    
+    print(arr)
+
+    for r in range(len(arr)):
+        # Varje kolumn
+        for c in range(len(arr[0])):
+            # Byt platsen på Kolumnen och raden från arry till outArr
+            outArr[c][r] = arr[r][c]
+
+ 
+    # För varje bokstav i matrisen så ska den lägga till det i outTxt
+    for idx in range(len(outArr)):
+        for val in outArr[idx]:
+            outTxt += val
+    
+    return outTxt        
 
 
-def transposeChipher(text, cipher):
-    pass
-
+   
 # Har argumenten "inmatade strängen" och en boolean ifall den ska kryptera eller dekryptera 
-def transpose(txt, cipher): 
+def transpose(txt): 
     # arr = [[""]* cols] * 3
     rows = 3
     cols = math.ceil(len(txt) / rows) # Anpassar antalet kolumner 
@@ -56,20 +91,14 @@ def transpose(txt, cipher):
         # print(arr, arr[rCount][cCount], txt[currentChar])    
     print(arr)
 
-    if cipher:
-        # För varje rad
-        for r in range(len(arr)):
-            # Varje kolumn
-            for c in range(len(arr[0])):
-                # Byt platsen på Kolumnen och raden från arry till outArr
-                outArr[c][r] = arr[r][c]
-    else:
-        for r in range(len(arr)):
-            # Varje kolumn
-            for c in range(len(arr[0])):
-                # Byt platsen på Kolumnen och raden från arry till outArr
-                outArr[c][r] = arr[r][c]
 
+    # För varje rad
+    for r in range(len(arr)):
+        # Varje kolumn
+        for c in range(len(arr[0])):
+            # Byt platsen på Kolumnen och raden från arry till outArr
+            outArr[c][r] = arr[r][c]
+    
     # För varje bokstav i matrisen så ska den lägga till det i outTxt
     for idx in range(len(outArr)):
         for val in outArr[idx]:
@@ -77,8 +106,11 @@ def transpose(txt, cipher):
     
     return outTxt        
 
-inT = "Hore llWdlo"
-lista = transpose(inT, False)
+transposeText = "Hello world"
+transposeReverse = "Hore llWdlo"
+
+encryptedText = transpose(transposeText)
+#decryptedText = transposeReverse(transposeReverse)
 
 # print(lista)
-print(lista)  
+print(encryptedText)  
