26cb482bf6d769bb32b1d25e2c8615d21f418247
Linus Jansson <linus.jansson2000@gmail.com>
2021-04-15 22:43:11 +0200

    Added a program loop

--- tillämpad programering/Kryptering/main.py
+++ tillämpad programering/Kryptering/main.py
@@ -1,9 +1,6 @@
 import caesarCipher as cc
 import transpositionCipher as tc
 
-
-inText = input("Skriv in ett ord eller flera för att kryptera > ")
-
 def handleUpper(txt):
     out = ""
     # Går igenom varje bokstav / tecken i strängen och gör om den till en lite bokstav
@@ -14,3 +11,29 @@ def handleUpper(txt):
             out += n
     
     return out
+
+textToEncrypt = handleUpper(input("Skriv in ett ord eller en mening ska hanteras > "))
+encryptOrDecrypt = bool(input("Ska det krypteras eller avkrypteras? (True eller False) > "))
+choosenProgram = input("Hur vill du köra? \n1 - Caesar Chiper\n2 - Transposition Chiper\nq - avsluta\n>> ")
+
+while True:
+    if choosenProgram == "1":
+        shiftAmount = int(input("Hur många steg ska varje bokstav skiftas med? > "))
+
+        encryptedText = cc.encrypt(textToEncrypt, shiftAmount)
+
+        print(f"Texten '{textToEncrypt}' krypterades till '{encryptedText}'")
+
+        break
+    elif choosenProgram == "2":
+        rows = int(input("Hur många rader ska det vara i matrisen? "))
+
+        encryptedText = tc.encrypt(textToEncrypt, rows, encryptOrDecrypt)
+
+        print(f"Texten '{textToEncrypt}' krypterades till '{encryptedText}'")
+
+    elif choosenProgram == "q":
+        break
+    else:
+        print("Detta program finns inte i listan av program")
+        choosenProgram = input("Hur vill du köra? \n1 - Caesar Chiper\n2 - Transposition Chiper\nq - avsluta\n>> ")
