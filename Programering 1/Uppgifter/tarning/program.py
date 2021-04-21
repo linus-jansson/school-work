598b84f40b1cd387c9907493609447fe8317559a
Linus J <linus.jansson2000@gmail.com>
2021-04-21 20:15:36 +0200

    Update program.py

--- Programering 1/Uppgifter/tarning/program.py
+++ Programering 1/Uppgifter/tarning/program.py
@@ -4,31 +4,32 @@ def genCombos(a, d):
 def numOfCombos(diceSides, n):
     return diceSides**n
 
-def generatePairSums(combinations):
+def generateTable(combinations):
     outDict = {}
     
     for pair in combinations:
         # print(pair, '->', x, '+', y, ' = ', x + y)
+        # Lägger till alla summor till tabellen om summan av de är samma som namnet på nyckeln i tabellen 
         outDict[str(sum(pair))] = [ [num for num in testingPairs] for testingPairs in combinations if (sum(testingPairs) == sum(pair))]
 
     return outDict
 
-        
 def printTable(key, combo):
-    print(key, combo)
+    print(f"Sum {key}: {'*' * len(combo)}")
 
+def main():
+    numOfSides = int(input("Hur många sidor finns det på tärningen? > "))
+    numOfDice = int(input("Hur många tärningar finns det? > "))
+    
+    combinations = genCombos(numOfSides, numOfDice)
+    print(combinations)
 
-numOfSides = 3
-numOfDice = 2
+    tabell = generateTable(combinations)
+    
+    print(f"{numOfDice} tärningar med {numOfSides} sidor kommer ge {numOfCombos(numOfSides, numOfDice)} kombinationer")
+    
+    for key, val in tabell.items():
+        printTable(key, val)
 
-print(genCombos(numOfSides, numOfDice))
-
-combinations = genCombos(numOfSides, numOfDice)
-
-tabell = generatePairSums(combinations)
-
-print(f"{numOfDice} tärningar med {numOfSides} sidor kommer ge {numOfCombos(numOfSides, numOfDice)} kombinationer")
-for key, val in tabell.items():
-    printTable(key, val)
-
-
+if __name__ == "__main__":
+    main()
