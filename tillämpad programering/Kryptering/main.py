82231bf3d7bcc6dc16e5db68c7b6ffe1ec8cfaca
Linus Jansson <linus.jansson2000@gmail.com>
2021-04-21 16:00:58 +0200

    Moved main part of code to a __main__ function

--- tillämpad programering/Kryptering/main.py
+++ tillämpad programering/Kryptering/main.py
@@ -1,43 +1,32 @@
 import caesarCipher as cc
 import transpositionCipher as tc
 
+def main():
+    textToEncrypt = input("Skriv in ett ord eller en mening ska hanteras > ")
+    encryptOrDecrypt = bool(input("Ska det krypteras(1) eller avkrypteras?(0) > "))
+    choosenProgram = input("Hur vill du köra? \n1 - Caesar Chiper\n2 - Transposition Chiper\nq - avsluta\n>> ")
 
-# ÄNDRA SÅ ATT DEN HANTERAR STORA BOKSTÄVER I CEASAR CHIPER
+    while True:
+        if choosenProgram == "1":
+            shiftAmount = int(input("Hur många steg ska varje bokstav skiftas med? > "))
 
+            encryptedText = cc.encrypt(textToEncrypt, shiftAmount)
 
-#def handleUpper(txt):
-#    out = ""
-#    # Går igenom varje bokstav / tecken i strängen och gör om den till en lite bokstav
-#    for n in txt:
-#        if n.isupper():
-#            out += chr(ord(n) + 32)
-#        else:
-#            out += n
-#    
-#    return out
+            print(f"Texten '{textToEncrypt}' krypterades till '{encryptedText}'")
 
-textToEncrypt = input("Skriv in ett ord eller en mening ska hanteras > ")
-encryptOrDecrypt = bool(input("Ska det krypteras(1) eller avkrypteras?(0) > "))
-choosenProgram = input("Hur vill du köra? \n1 - Caesar Chiper\n2 - Transposition Chiper\nq - avsluta\n>> ")
+            break
+        elif choosenProgram == "2":
+            rows = int(input("Hur många rader ska det vara i matrisen? "))
 
-while True:
-    if choosenProgram == "1":
-        shiftAmount = int(input("Hur många steg ska varje bokstav skiftas med? > "))
+            encryptedText = tc.encrypt(textToEncrypt, rows, encryptOrDecrypt)
 
-        encryptedText = cc.encrypt(textToEncrypt, shiftAmount)
+            print(f"Texten '{textToEncrypt}' krypterades till '{encryptedText}'")
 
-        print(f"Texten '{textToEncrypt}' krypterades till '{encryptedText}'")
+        elif choosenProgram == "q":
+            break
+        else:
+            print("Detta program finns inte i listan av program")
+            choosenProgram = input("Hur vill du köra? \n1 - Caesar Chiper\n2 - Transposition Chiper\nq - avsluta\n>> ")
 
-        break
-    elif choosenProgram == "2":
-        rows = int(input("Hur många rader ska det vara i matrisen? "))
-
-        encryptedText = tc.encrypt(textToEncrypt, rows, encryptOrDecrypt)
-
-        print(f"Texten '{textToEncrypt}' krypterades till '{encryptedText}'")
-
-    elif choosenProgram == "q":
-        break
-    else:
-        print("Detta program finns inte i listan av program")
-        choosenProgram = input("Hur vill du köra? \n1 - Caesar Chiper\n2 - Transposition Chiper\nq - avsluta\n>> ")
+if __name__ == "__main__":
+    main()
